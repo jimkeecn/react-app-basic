a4a068f97f246f9d301f16d40e9e5d94c78451a4
jimkeecn <jimkeecn@gmail.com>
2019-06-15 16:14:07 +1000

    binding event handlers

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -6,8 +6,15 @@ class Counter extends Component {
     tags: ["tag1", "tag2", "tag3"]
   };
 
+  constructor() {
+    super();
+    this.handleIncrement = this.handleIncrement.bind(this);
+  }
+
   handleIncrement() {
     console.log("increment clicked", this);
+    // obj.method();
+    // function();
   }
   render() {
     return (
