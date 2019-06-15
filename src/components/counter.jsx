c577a61e0d58a44fde57b436321b8ca66bd8ea16
jimkeecn <jimkeecn@gmail.com>
2019-06-15 17:37:16 +1000

    passing Children (two ways)

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -11,8 +11,12 @@ class Counter extends Component {
   };
 
   render() {
+    console.log(this.props);
+
     return (
       <div>
+        <h4>{this.props.id}</h4>
+        {this.props.children}
         <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
         <button
           onClick={() => this.handleIncrement({ id: 1 })}
