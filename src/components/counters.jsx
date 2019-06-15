3946a874bf1282e6de6cb9a6cab74bdb4a322147
jimkeecn <jimkeecn@gmail.com>
2019-06-15 19:11:15 +1000

    Destructuring Arguments

--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -5,20 +5,18 @@ import Counter from "./counter";
     */
 class Counters extends Component {
   render() {
+    const { onReset, counters, onDelete, onIncrement } = this.props;
     return (
       <div>
-        <button
-          className="btn btn-primary btn-sm m-2"
-          onClick={this.props.onReset}
-        >
+        <button className="btn btn-primary btn-sm m-2" onClick={onReset}>
           reset
         </button>
-        {this.props.counters.map(counter => (
+        {counters.map(counter => (
           <Counter
             key={counter.id} //key is an internal use key value by React
             counter={counter}
-            onDelete={this.props.onDelete}
-            onIncrement={this.props.onIncrement}
+            onDelete={onDelete}
+            onIncrement={onIncrement}
           />
         ))}
       </div>
--- src/components/navBar.jsx
+++ src/components/navBar.jsx
@@ -1,14 +1,14 @@
 import React, { Component } from "react";
 
 //Stateless functional Component
-const NavBar = props => {
+const NavBar = ({ totalCountersHasValue }) => {
   // have to pass the props into stateless functional components
   return (
     <nav className="navbar navbar-light bg-light">
       <a className="navbar-brand" href="#">
         NavBar{" "}
         <span className="badge badge-pull badge-secondary">
-          {props.totalCountersHasValue}
+          {totalCountersHasValue}
         </span>
       </a>
     </nav>
